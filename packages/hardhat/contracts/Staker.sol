c79bc4368988806817c701c86409f5d175ef9158
Vedant Joshi <vedantjoshi@192.168.1.11>
2024-11-18 07:53:06 +0530

    Complete

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
 
-import "hardhat/console.sol";
+
 import "./ExampleExternalContract.sol";
+
 
 contract Staker {
 
@@ -12,8 +13,69 @@ contract Staker {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+  mapping ( address => uint256 ) public balances;
+
+  uint256 public constant threshold = 1 ether;
+
+  uint256 public  deadline = block.timestamp + 72 hours;
+
+  bool openForWithdraw = false;
+
+  bool callOnlyOnce = false;
+
+
+  event Stake(address,uint256);
+
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
+
+  function timeLeft() public view returns (uint256) {
+  
+    if(block.timestamp >= deadline) {
+        return 0;
+    }
+    if(block.timestamp < deadline) {
+        return (deadline - block.timestamp);  // Fixed: deadline minus current time
+    }
+}
+
+  function  stake() public payable {
+    address from = msg.sender;
+    balances[from] += msg.value;
+
+    emit Stake(msg.sender, msg.value);
+  }
+
+
+  function execute() public {
+     require(block.timestamp >= deadline, "Still Time is Remaining");
+     require(!callOnlyOnce, "Already Been Called Once");
+
+     callOnlyOnce = true;
+
+     if(address(this).balance >= threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+      return;
+     }
+
+     openForWithdraw = true;
+
+  }
+
+  function withdraw() public {
+    require(openForWithdraw,"Funds Transfered to external contract");
+    require(balances[msg.sender] > 0,"No Amount Remaining");
+    uint toTransfer = balances[msg.sender];
+    balances[msg.sender] = 0;
+    payable(msg.sender).transfer(toTransfer);
+  }
+
+  receive() external payable {
+    stake();
+    // balances[msg.sender] += msg.value;
+  }
+
+     
 
 
   // After some `deadline` allow anyone to call an `execute()` function
